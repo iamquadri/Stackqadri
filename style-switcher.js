34f66ce89a2a81d53ae4058674ffbe5587f0dbfb
iamquadri <quadrihosein69@gmail.com>
2023-09-05 18:34:38 +0100

    stackqadri portfolio with JavaScript and css

---
+++ style-switcher.js
@@ -0,0 +1,31 @@
+
+
+/***************** toogle style switcher ********** */
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
+// hide style -switcher on sroll
+window.addEventListener("scroll", () => {
+    if(document.querySelector(".style-switcher").classList.contains("open"))
+    {
+        document.querySelector(".style-switcher").classList.remove("open");
+    }
+})
+
+// /*------------------------- theme colors -------------------------*
+
+const alternateStyles = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color)
+{
+    alternateStyles.forEach((style) => {
+        if(color === style.getAttribute("title"))
+        {
+            style.removeAttribute("disabled");
+        }
+        else
+        {
+            style.setAttribute("disabled","true");
+        }
+    })
+}
